f7678cd85b66914039a3d63c34ac253720dd7007
Alexis Dorado <alexisdorado@unicauca.edu.co>
2021-04-19 01:42:08 -0500

    Mejora de rendimiento en generar excel

--- controllers/catalogo/generar_formato.controlador.js
+++ controllers/catalogo/generar_formato.controlador.js
@@ -31,6 +31,8 @@ const generarExcel = async (req, res = response) => {
   }
 
 
+  var arrCodigosImagenesNoAsociadas = [];
+  var arrImagenesNoAsociadas = [];
   //Buscar imagenes de estampillas del catálogo
   const imagenesEstampillasBD = await ImagenesEstampillas.aggregate(
     [
@@ -43,164 +45,149 @@ const generarExcel = async (req, res = response) => {
           as: "arrEstampilla"
         }
       },
-
      {
        $project: {
         catalogo:1,
-         arrEstampilla: { $arrayElemAt: ["$arrEstampilla", 0] },
-         codigo_estampilla:1
+         estampillaAsociada: { $arrayElemAt: ["$arrEstampilla", 0] },
+         codigo_estampilla:1,
+         imagen_url:1
        }
      },
      {
        $match: {
         catalogo: ObjectId(id_catalogo)
        }
-     }
+     },
+     
 
     ]
     );
 
+    imagenesEstampillasBD.map((data) => {
+      if (!data.estampillaAsociada){
+        
+        
+        arrCodigosImagenesNoAsociadas.push(data.codigo_estampilla);
+        arrImagenesNoAsociadas.push(data);
+      }
+    });
     console.log("imagenesEstampillasBD -> ",imagenesEstampillasBD.length);
-
-  // //Buscando imagenes no asociadas a estampillas
-  // if (imagenesEstampillasBD != null) {
-  //   var codigosEs = [];
-
-  //   imagenesEstampillasBD.map(data =>
-  //     {
-  //       codigosEs.push(data.codigo_estampilla);
-  //     });
-    
-  //   var imagenesNoAsociadas = await ImagenesEstampillas.find( { CODIGO: codigosEs } );
-  //   console.log("imagenesNoAsociadas ->", imagenesNoAsociadas);
+    console.log("imagenesNoAsociadas -> ", arrCodigosImagenesNoAsociadas);
 
 
-  
-  // }else{
-  //   return res.json({
-  //     ok:false,
-  //     msg: "No es posible generar el excel, debes agregar imagenes antes de generar el documento."
-    
-  //   })
-  // }
+  const [, , filename] = process.argv;
 
+  const wb = new Excel.Workbook();
+  const ws = wb.addWorksheet("Formato catalogo");
 
+  const letrasColumnas = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+  ];
 
+  //agregando titulos de las columnas
+  ws.columns = [
+    { header: "NUMERO", key: "id", width: 10 },
+    { header: "CODIGO", key: "id", width: 40 },
+    { header: "FOTO_ESTAMPILLAS", key: "id", width: 20 },
+    { header: "DESCRIPCION_ESTAMPILLA", key: "id", width: 30 },
+    { header: "NRO_ESTAMPILLAS", key: "id", width: 20 },
+    { header: "CATEGORIA", key: "id", width: 15 },
+    { header: "NUMERO_DE_CATALOGO", key: "id", width: 25 },
+    { header: "TIPO", key: "id", width: 15 },
+    { header: "ANIO", key: "id", width: 10 },
+    { header: "GRUPO", key: "id", width: 10 },
+    { header: "TITULO_DE_LA_SERIE", key: "id", width: 40 },
+    { header: "VALOR_FACIAL", key: "id", width: 15 },
+    { header: "TIPO_MONEDA_VALOR_FACIAL", key: "id", width: 30 },
+    { header: "VALOR_CATALOGO_NUEVO", key: "id", width: 26 },
+    { header: "VALOR_DEL_CATALOGO_USADO", key: "id", width: 30 },
+    { header: "MONEDA_VALOR_CATALOGO_NUEVO_USADO", key: "id", width: 43 },
+    { header: "VARIANTES_ERRORES", key: "id", width: 40 },
+    { header: "FOTO_VARIANTES_ERRORES", key: "id", width: 40 },
+  ];
 
+  //Asignando total de imagenes a mostrar
+  var totalEstampillas = arrCodigosImagenesNoAsociadas.length;
 
-  // console.log("Imagenes catalogo -> ", imagenesEstampillasBD.length);
+  //Centrando contenido de las columnas
+  letrasColumnas.map((datos, i) => {
+    ws.getCell(datos + "1").alignment = {
+      vertical: "center",
+      horizontal: "center",
+    };
+  });
 
-  // const [, , filename] = process.argv;
+  //agregando ids al excel
+  var contador = 0;
+  for (let index = 2; index < totalEstampillas + 2; index++) {
+    contador = contador + 1;
+    ws.getRow(index).values = [
+      contador,
+      arrCodigosImagenesNoAsociadas[index - 2].codigo_estampilla,
 
-  // const wb = new Excel.Workbook();
-  // const ws = wb.addWorksheet("Formato catalogo");
+    ];
+  }
 
-  // const letrasColumnas = [
-  //   "A",
-  //   "B",
-  //   "C",
-  //   "D",
-  //   "E",
-  //   "F",
-  //   "G",
-  //   "H",
-  //   "I",
-  //   "J",
-  //   "K",
-  //   "L",
-  //   "M",
-  //   "N",
-  //   "O",
-  //   "P",
-  //   "Q",
-  //   "R",
-  // ];
+  for (let index = 1; index < totalEstampillas + 2; index++) {
+    letrasColumnas.map((datos, i) => {
+      ws.getCell(datos + index).border = {
+        top: { style: "thin", color: { argb: "ffd7d7d7" } },
+        left: { style: "thin", color: { argb: "ffd7d7d7" } },
+        bottom: { style: "thin", color: { argb: "ff696969" } },
+        right: { style: "thin", color: { argb: "ffd7d7d7" } },
+      };
+    });
+  }
 
-  // //agregando titulos de las columnas
-  // ws.columns = [
-  //   { header: "NUMERO", key: "id", width: 10 },
-  //   { header: "CODIGO", key: "id", width: 40 },
-  //   { header: "FOTO_ESTAMPILLAS", key: "id", width: 20 },
-  //   { header: "DESCRIPCION_ESTAMPILLA", key: "id", width: 30 },
-  //   { header: "NRO_ESTAMPILLAS", key: "id", width: 20 },
-  //   { header: "CATEGORIA", key: "id", width: 15 },
-  //   { header: "NUMERO_DE_CATALOGO", key: "id", width: 25 },
-  //   { header: "TIPO", key: "id", width: 15 },
-  //   { header: "ANIO", key: "id", width: 10 },
-  //   { header: "GRUPO", key: "id", width: 10 },
-  //   { header: "TITULO_DE_LA_SERIE", key: "id", width: 40 },
-  //   { header: "VALOR_FACIAL", key: "id", width: 15 },
-  //   { header: "TIPO_MONEDA_VALOR_FACIAL", key: "id", width: 30 },
-  //   { header: "VALOR_CATALOGO_NUEVO", key: "id", width: 26 },
-  //   { header: "VALOR_DEL_CATALOGO_USADO", key: "id", width: 30 },
-  //   { header: "MONEDA_VALOR_CATALOGO_NUEVO_USADO", key: "id", width: 43 },
-  //   { header: "VARIANTES_ERRORES", key: "id", width: 40 },
-  //   { header: "FOTO_VARIANTES_ERRORES", key: "id", width: 40 },
-  // ];
-  // var totalEstampillas = imagenesEstampillasBD.length;
+  for (let index = 1; index < totalEstampillas + 1; index++) {
+    ws.addImage(
+      wb.addImage({
+        filename: path.join(
+          __dirname,
+          "../../uploads/" + arrImagenesNoAsociadas[index - 1].imagen_url
+        ),
+        extension: "png",
+      }),
 
-  // //Centrando contenido de las columnas
-  // letrasColumnas.map((datos, i) => {
-  //   ws.getCell(datos + "1").alignment = {
-  //     vertical: "center",
-  //     horizontal: "center",
-  //   };
-  // });
+      {
+        // tl: { col: 1, row: 1 },
+        tl: "B" + index,
+        ext: { width: 140, height: 140 },
+      }
+    );
+    ws.getRow(index + 1).height = 105;
+  }
 
-  // //agregando ids al excel
-  // var contador = 0;
-  // for (let index = 2; index < totalEstampillas + 2; index++) {
-  //   contador = contador + 1;
-  //   ws.getRow(index).values = [
-  //     contador,
-  //     imagenesEstampillasBD[index - 2].codigo_estampilla,
-
-  //   ];
-  // }
-
-  // for (let index = 1; index < totalEstampillas + 2; index++) {
-  //   letrasColumnas.map((datos, i) => {
-  //     ws.getCell(datos + index).border = {
-  //       top: { style: "thin", color: { argb: "ffd7d7d7" } },
-  //       left: { style: "thin", color: { argb: "ffd7d7d7" } },
-  //       bottom: { style: "thin", color: { argb: "ff696969" } },
-  //       right: { style: "thin", color: { argb: "ffd7d7d7" } },
-  //     };
-  //   });
-  // }
-
-  // for (let index = 1; index < totalEstampillas + 1; index++) {
-  //   ws.addImage(
-  //     wb.addImage({
-  //       filename: path.join(
-  //         __dirname,
-  //         "../../uploads/" + imagenesEstampillasBD[index - 1].imagen_url
-  //       ),
-  //       extension: "png",
-  //     }),
-
-  //     {
-  //       // tl: { col: 1, row: 1 },
-  //       tl: "B" + index,
-  //       ext: { width: 140, height: 140 },
-  //     }
-  //   );
-  //   ws.getRow(index + 1).height = 105;
-  // }
-
-  // wb.xlsx
-  //   .writeFile(
-  //     path.join(__dirname, "../../uploads/documentos/formato_filatelia.xlsx")
-  //   )
-  //   .then(() => {
-  //     console.log("Done.");
-  //     res.download(
-  //       path.join(__dirname, "../../uploads/documentos/formato_filatelia.xlsx")
-  //     );
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.message);
-  //   });
+  wb.xlsx
+    .writeFile(
+      path.join(__dirname, "../../uploads/documentos/formato_filatelia.xlsx")
+    )
+    .then(() => {
+      console.log("Done.");
+      res.download(
+        path.join(__dirname, "../../uploads/documentos/formato_filatelia.xlsx")
+      );
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 };
 
 module.exports = {
